ea72b502eacaf555314dd9b021df639a6f5b8226
Jakub Czesak <jakub.czesak@cbre.com>
2023-02-04 18:49:34 +0100

    Add Accordion JS

--- src/js/script.js
+++ src/js/script.js
@@ -62,6 +62,8 @@
       thisProduct.renderInMenu();
       console.log('new Product: ', thisProduct);
 
+      thisProduct.initAccordion();
+
     }
 
     renderInMenu(){
@@ -79,7 +81,38 @@
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
     }
+    
+    initAccordion(){
+      const thisProduct = this;
+  
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = this.element.querySelector(select.menuProduct.clickable);
+      console.log('clickableTrigger: ', clickableTrigger);
+  
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+        event.preventDefault();
+
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector('article.active');
+        console.log('activePRODUCT: ', activeProduct);
+  
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if(activeProduct != null && activeProduct != thisProduct.element){
+          activeProduct.classList.remove('active');
+          console.log('removed');
+        }
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle('active');
+      });
+    }
+
+
+    
+
   }
+  
 
   const app = {
     initMenu: function(){
